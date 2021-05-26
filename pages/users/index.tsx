5d073d6b2826eff104aadefc4c77f8f2051b021f
mamedaifuku <hiroshikato1208@gmail.com>
2021-05-26 23:33:23 +0900

    console.logを削除

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -4,24 +4,20 @@ import { sampleUserData } from '../../utils/sample-data';
 import Layout from '../../components/Layout';
 import List from '../../components/List';
 
-const UserList = () => {
-  console.log(sampleUserData);
-
-  return (
-    <Layout title='Users List | Next.js + TypeScript Example'>
-      <h1>Users List</h1>
-      <p>
-        Example fetching data from inside <code>getStaticProps()</code>.
-      </p>
-      <p>You are currently on: /users</p>
-      <List items={sampleUserData} />
-      <p>
-        <Link href='/'>
-          <a>Go home</a>
-        </Link>
-      </p>
-    </Layout>
-  );
-};
+const UserList = () => (
+  <Layout title='Users List | Next.js + TypeScript Example'>
+    <h1>Users List</h1>
+    <p>
+      Example fetching data from inside <code>getStaticProps()</code>.
+    </p>
+    <p>You are currently on: /users</p>
+    <List items={sampleUserData} />
+    <p>
+      <Link href='/'>
+        <a>Go home</a>
+      </Link>
+    </p>
+  </Layout>
+);
 
 export default UserList;
